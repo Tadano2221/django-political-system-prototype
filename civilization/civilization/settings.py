69152f3d641991a318273f78bf509205b32edc21
Nyan Lin Ko <90366198+Tadano2221@users.noreply.github.com>
2026-08-06 20:04:10 +0630

    Update settings.py

--- civilization/civilization/settings.py
+++ civilization/civilization/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-er2nkq$y%u9avn@f$f180al2eshjh17$1)(u^aplfo!^#yq+6%"
+SECRET_KEY = #ADD YOUR SECRET KEY HERE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
